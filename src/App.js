355427fd7dfc43d418e88d5046612208bf508860
mefss <mef_ss@yahoo.com>
2022-07-27 13:22:18 +0430

    commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ const App = () => {
   return (
     <div className="App">
       <h3>اپلیکیشن مدیریت مخاطبین</h3>
+      <button className='btn btn-primary'>Button</button>
+      <br/>
+      <span className='far fa-user'></span>
+      <br/>
+      <span className='fas fa-address-book'></span>
     </div>
   );
 }
