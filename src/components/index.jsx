4ecbf8e97a69333b34d0bf0ec22d63c30b1f45ad
mefss <mef_ss@yahoo.com>
2022-09-10 16:34:14 +0430

    commit

---
+++ src/components/index.jsx
@@ -0,0 +1,10 @@
+//Contact Components
+export {default as AddContact} from '../components/Contacts/AddContact';
+export {default as EditContact} from '../components/Contacts/EditContact';
+export {default as ViewContact} from '../components/Contacts/ViewContact';
+export {default as SearchContact} from '../components/Contacts/SearchContact';
+export {default as Contact} from '../components/Contacts/Contact';
+export {default as Contacts} from '../components/Contacts/Contacts';
+
+//Other Components
+export {default as Navbar} from './Navbar';
